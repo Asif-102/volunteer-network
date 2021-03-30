a53deff679181302488c1edb7159ca2bc1db1851
Asif2535 <aiqbal517@gmail.com>
2021-03-30 12:48:40 +0600

    first commit

---
+++ src/components/Event/Event.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Event = ({event}) => {
+    const deleteEvent = id =>{
+        const url=`http://localhost:5000/deleteEvent/${id}`;
+        fetch(url,{
+            method:'DELETE',
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then(res => setTimeout(window.location.reload(true),4000))
+        
+    }
+    return (
+        <div className="col-md-3">
+            <img style={{height: '300px'}} src={event.imageURL} alt=""/>
+            <h3>{event.name} <button onClick={()=>deleteEvent(event._id)}>Delete</button></h3>
+        </div>
+    );
+};
+
+export default Event;
