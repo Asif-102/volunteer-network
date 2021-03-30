27ae1b100d0dacbbbcf0dab0281f79543bbfb7cc
Asif2535 <aiqbal517@gmail.com>
2021-03-30 12:58:35 +0600

    fixed setTimeout time

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -11,7 +11,7 @@ const Event = ({event}) => {
         })
         .then(res => setTimeout(()=>{
             window.location.reload(true)
-        },3000))
+        },2000))
         
     }
     return (
